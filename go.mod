671fc67d46952e5742b19069f69629a8f077e147
Morpa <andremorpa@gmail.com>
2024-01-17 19:17:14 +0000

    add sqlite database

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/Morpa/htmx-go-todo
 go 1.21.6
 
 require github.com/go-chi/chi/v5 v5.0.11
+
+require github.com/mattn/go-sqlite3 v1.14.19
--- main.go
+++ main.go
@@ -2,19 +2,32 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
 
+	"github.com/Morpa/htmx-go-todo/internal/db"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
-func main(){
-	r:= chi.NewRouter()
+func main() {
+	err := db.OpenDB()
+	if err != nil {
+		log.Panic(err)
+	}
+	defer db.CloseDB()
+
+	err = db.SetupDB()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 	r.Handle("/static/*", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-	r.Get("/", func(w http.ResponseWriter, _ *http.Request){
-		tmpl,_:=template.New("").ParseFiles("templates/index.html")
-		tmpl.ExecuteTemplate(w,"Base", nil)
+	r.Get("/", func(w http.ResponseWriter, _ *http.Request) {
+		tmpl, _ := template.New("").ParseFiles("templates/index.html")
+		tmpl.ExecuteTemplate(w, "Base", nil)
 	})
 	http.ListenAndServe(":3000", r)
 }
